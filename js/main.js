eb73eeb133d8717d481a972f19764416859b67b8
Natasha <natashasosa1012@gmail.com>
2024-03-31 18:28:20 -0600

    Se terminó la lista de compras

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// El código va aquí -> 
 let btnAgregar = document.getElementById("btnAgregar");
 let btnClear = document.getElementById("btnClear");
 let txtNombre = document.getElementById("Name");
@@ -8,7 +7,8 @@ let alertValidaciones = document.getElementById("alertValidaciones");
 let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
 let tablaListaCompras = document.getElementById("tablaListaCompras");
 let cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
-//Limpia toda la lista de compras incluyendo los campos
+
+
 let contadorProductos = document.getElementById("contadorProductos");
 let productosTotal = document.getElementById("productosTotal");
 let precioTotal = document.getElementById("precioTotal");
@@ -22,6 +22,7 @@ let totalEnProductos = 0;
 
 let datos = new Array();
 
+//LIMPIAR TODO BOTÓN CLEAR
 btnClear.addEventListener("click", function (event) {
     event.preventDefault();
     txtNombre.value = "";
@@ -34,7 +35,8 @@ btnClear.addEventListener("click", function (event) {
     costoTotal = 0;
     totalEnProductos = 0;
     contadorProductos.innerText = contador;
-    productosTotal.innerText = `$ ${costoTotal.toFixed(2)}`;
+    productosTotal.innerText = totalEnProductos;
+    precioTotal.innerText = `$ ${costoTotal.toFixed(2)}`;
     localStorage.setItem("contadorProductos", contador);
     localStorage.setItem("totalEnProductos", totalEnProductos);
     localStorage.setItem("costoTotal", costoTotal);
@@ -42,7 +44,8 @@ btnClear.addEventListener("click", function (event) {
     datos = new Array();
     cuerpoTabla.innerHTML = "";
     txtNombre.focus();
-});//btn CLear
+});//LIMPIAR TODO BOTÓN CLEAR
+
 
 function validarCantidad() {
 
@@ -61,13 +64,13 @@ function validarCantidad() {
     }
 
     return true;
-}//Validar cantidad
+}
 
 function getPrecio() {
     return parseInt((Math.random() * 75) * 100) / 100
 
 }
-
+//BOTÓN AGREGAR
 btnAgregar.addEventListener("click", function (event) {
     event.preventDefault();
     alertValidacionesTexto.innerHTML = "";
@@ -75,22 +78,27 @@ btnAgregar.addEventListener("click", function (event) {
     txtNombre.style.border = "";
     txtNumber.style.border = "";
     isValid = true;
-    txtNombre.value = txtNombre.value.trim();
+    txtNombre.value = txtNombre.value.trim();//TRIM SIRVE PARA EVITAR VALIDACIONES CON ESPACIOS
     txtNumber.value = txtNumber.value.trim();
+    //BOTÓN AGREGAR
+
+    //VALIDAR LONGITUD MAYOR QUE 3
     if (txtNombre.value.length < 3) {
-        alertValidacionesTexto.insertAdjacentHTML("beforeend", `El <strong>Nombre </strong> no es correcto <br/>`);
+        alertValidacionesTexto.insertAdjacentHTML("beforeend", `El <strong>nombre </strong> no es correcto <br/>`);
         alertValidaciones.style.display = "block";
         txtNombre.style.border = "solid red thin";
         isValid = false;
-    }// si la longitud es menor que tres
+    }//VALIDAR LONGITUD MAYOR QUE 3
 
+    //VALIDAR QUE SEA UN NÚMERO Y LA LONGITUD
     if (!validarCantidad()) {
         alertValidacionesTexto.insertAdjacentHTML("beforeend", `La <strong> cantidad </strong> no es correcta <br/>`);
         alertValidaciones.style.display = "block";
         txtNumber.style.border = "solid red thin";
         isValid = false;
-    }
+    }//VALIDAR QUE SEA UN NÚMERO Y LA LONGITUD
 
+    //AGREGAR ELEMENTOS A LA TABLA DE COMPRAS
     if (isValid) {
         contador++;
         precio = getPrecio();
@@ -102,22 +110,31 @@ btnAgregar.addEventListener("click", function (event) {
         </tr>
         `;
 
-
         let elemento = `{"id" : ${contador},
                         "nombre" : "${txtNombre.value}",
                         "cantidad" : ${txtNumber.value},
                         "precio" : ${precio}
-    }`;
+
+        }`;
         datos.push(JSON.parse(elemento));
         console.log(datos);
-        localStorage.setItem("datos");
+        localStorage.setItem("datos", JSON.stringify(datos));
+
         cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        //AGREGAR ELEMENTOS A LA TABLA DE COMPRAS ^
         contadorProductos.innerText = contador;
+        //CONTADOR DE PRODUCTOS ^
         totalEnProductos += parseFloat(txtNumber.value);
+        //SUMA DE PRODUCTOS ^
         productosTotal.innerText = totalEnProductos;
+        //SUMA DE PRODUCTOS ^
         costoTotal += precio * parseFloat(txtNumber.value);
         precioTotal.innerText = `$ ${costoTotal.toFixed(2)}`;
-
+        //SUMA DE PRECIOS ^ 
+        txtNombre.value = "";
+        txtNumber.value = "";
+        txtNombre.focus();
+        //VACIAR CAMPOS CUANDO SE AGREGAN PRODUCTOS ^
 
         localStorage.setItem("contadorProductos", contador);
         localStorage.setItem("totalEnProductos", totalEnProductos);
@@ -128,14 +145,18 @@ btnAgregar.addEventListener("click", function (event) {
         txtNombre.focus();
     }//if isValid
 });//btnAgregar
+
+//MANTENER LOCALSTORAGE
 window.addEventListener("load", function (event) {
     event.preventDefault();
     if (this.localStorage.getItem("contadorProductos") != null) {
+        contador = Number(this.localStorage.getItem("contadorProductos"));
         totalEnProductos = Number(this.localStorage.getItem("totalEnProductos"));
         costoTotal = Number(this.localStorage.getItem("costoTotal"));
+
         contadorProductos.innerText = contador;
         productosTotal.innerText = totalEnProductos;
-        precioTotal.innerText = `${costoTotal.toFixed(2)}`;
+        precioTotal.innerText = `$ ${costoTotal.toFixed(2)}`;
     } //if !=null
 
     if (this.localStorage.getItem("datos") != null) {
